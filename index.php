913b37005479128261ce46db5e7711f6641b7313
januari12 <januari.yanti@student.untan.ac.id>
2018-03-21 08:50:29 +0700

    Latihan 1 Halaman Pertama

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//ini adalah komentar
+
+	echo "Rahmah Meilany Efendy<br>";
+	echo "H1101161031<br>";
+
+	echo "Hello world!<br>";
+	echo "Ini adalah duplikat";
+	echo "Ini adalah duplikat";
+	echo "Ini adalah duplikat";
+	echo "Ini adalah duplikat";
+	echo "Ini adalah duplikat";
+
+	$aa=175;
+	echo "<br>Tinggi badan dia adalah" .$aa. "cm";
+	
+?>
